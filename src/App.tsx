032943a6c7946fff04b5a6c11857fe1bccfc582e
masaosetogawa <ms.job.hunting@gmail.com>
2020-04-23 16:09:40 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>都道府県</th>
+            <th>市町村</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>北海道</td>
+            <td>釧路市</td>
+          </tr>
+          <tr>
+            <td>東京都</td>
+            <td>八王子市</td>
+          </tr>
+          <tr>
+            <td>沖縄県</td>
+            <td>那覇市</td>
+          </tr>
+          <tr>
+            <td>静岡県</td>
+            <td>熱海市</td>
+          </tr>
+          <tr>
+            <td>神奈川県</td>
+            <td>横浜市</td>
+          </tr>
+          <tr>
+            <td>福井県</td>
+            <td>福井市</td>
+          </tr>
+          <tr>
+            <td>富山県</td>
+            <td>富山市</td>
+          </tr>
+          <tr>
+            <td>石川県</td>
+            <td>金沢市</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default App;
